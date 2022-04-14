887bfb4ef262def7dd6075291d5f30bf3e95d199
Rai <robertblake.hardy@outlook.com>
2022-04-14 23:26:29 +0000

    chore: api.route cleanup

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -3,16 +3,8 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const api = require('../controllers/api.controller');
 
-router.route('/')
-    .get()
-
-router.route('/c')
-    .get(api.showAPI)
-
 router.route('/c/:name')
     .get(catchAsync(api.showCommunityAPI))
-
-router.route('/u')
 
 router.route('/u/:username')
     .get(catchAsync(api.showUserAPI))
